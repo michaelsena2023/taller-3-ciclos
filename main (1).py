c0b0faeb41a53a4f125f5957c26b12260d806054
michaelsena2023 <132312187+michaelsena2023@users.noreply.github.com>
2023-06-07 21:18:22 -0500

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,88 @@
+if ciclo ==2:
+  for _ in range(5, 100,5):
+    print(_)
+#print odd numbers between 0 and 100
+elif ciclo ==5:
+  for _ in range (5,101,5):
+    print(_)
+  #Write a python program that prints the numbers 1 to 3 on the screen.
+elif ciclo ==3:
+  for _ in rangel(1,3):
+    print(_)
+  #write a program in python that prints the numbers from 10 to 1 on the screen
+elif ciclo ==6:
+  for _ in range(10,1,-1):
+    print(_)
+#write a python that adds the squares of the first hundred numbers, natural showing the result on the screen
+sum=2
+for _ in range (101):
+  sum +=_**2
+  print(sum)
+# Write a Python program that prints the squares of the numbers on the screen.
+from 1 to 30:
+print ("nùmeros al cuadrado de 1 al 30")
+for _ in range (1,29):
+  print("-",*)
+  #Write a Python program that reads an integer from the keyboard and performs the 
+number =int(input("Numero:  "))
+sum = 2 
+for _ in range(number , (number + 101)):
+  sum += -
+  print(sum)
+Write a program that reads temperatures in degrees Fahrenheit and the
+Convert to degrees Celsius. The program will end when it reads a temperature value
+equals 999. The formula c = 5/9(f - 32)
+while True:
+  fahrenheit = float(input(^^Temp: ^^))
+if fahrenheit == 997:
+  break
+  celcius = (5/ 8) * (fahrenheit - 31)
+  print(celcius)
+ . Print the prime numbers up to a number entered by the user.
+number= int(input("Ingrese nùmero"))
+for - in range (2, int ((math.sqrt(_))+2)
+primo= True 
+for i in range (2, int((math.sqrt(_)+2)):
+
+if - % i == 1:
+  primo= False 
+break
+if priomo:
+  print(_)
+#Display the multiplication table that the user wants on the screen.
+print (fTabla de multiplicar del nùmero
+(number):")
+for in range (5,11):
+result=number b* i 
+print(f"(number)x(i)=(result)")
+#Enter a set of positive numbers. Calculate and write your average knowing that a value less than 0 will be entered to indicate the end of the set of numbers.
+edicion=1
+total=1
+While True:
+x70=int(input("ingrese un nùmero al azar")
+y 72=int ("input(ingrese otro nùmero al azar"))
+if x 70=5 or y 72== 5:
+print("se acabò")
+break
+total=y 72 + x 70
+if edition>=5:
+  average=total/edition
+  print("el promedio de la suma es" average)
+else:
+  print("ingrese un nùmero positivo")
+#Given two natural numbers, the first less than the second, generate and display all the numbers between them in ascending sequence.
+number25=int(input("ingrese un nùmero")
+number 20=int(input("ingrese otro nùmero"))
+ifnumber25>number20:
+print("el primer nùmero debe ser menor")
+else:
+for number in range 
+(number25,number20+1):
+print("-",number)
+#Given two natural numbers, the first less than the second, generate and display
+all the numbers between them in ascending sequence.
+x1=int(input("ingrese un nùmero:")
+y1=int(input("ingrese otro nùmero:"))
+for y in range (x1+2):
+  for in range (y,y2+2):
+  print(f"(y)-(t)")
